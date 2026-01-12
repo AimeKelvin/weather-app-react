d7caf2904fe2ef94d21ad48241c075e759a4e0da
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2026-01-12 15:38:24 +0200

    PWA setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // automatically updates the service worker
+      manifest: {
+        name: "Aime Weather",
+        short_name: "Weather",
+        description: "Real-time weather updates at your fingertips.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#3b82f6",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [], // no offline caching
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
